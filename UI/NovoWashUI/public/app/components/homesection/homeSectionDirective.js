46fa4e3b03d50eea49b012f119643d6dbe9a7742
manishkm <manishkm@dewsolutions.in>
2017-08-04 01:42:47 +0530

    delete UI from here

--- UI/NovoWashUI/public/app/components/homesection/homeSectionDirective.js
+++
@@ -1,8 +0,0 @@
-app
-    .directive('homeSection', function() {
-    return ({
-        restrict: 'E',
-        transclude: 'true',
-        templateUrl: 'app/components/homesection/homesection.html'
-    });
-});
